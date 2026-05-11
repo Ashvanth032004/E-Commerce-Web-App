febe74ab7656b40e3fac67ef0cb9b9ae8d794956
Ashvanth Kumar J <ashvanthkumar.j@gmail.com>
2026-05-11 22:49:14 +0530

    initial commit

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,7 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
@@ -17,26 +15,19 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
 
-        String browser = ConfigReader.getBrowser();
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
+
         driver.manage().timeouts()
-                .implicitlyWait(Duration.ofSeconds(ConfigReader.getTimeout()));
+                .implicitlyWait(Duration.ofSeconds(10));
 
         driver.get(ConfigReader.getBaseUrl());
     }
 
     @AfterMethod
     public void tearDown() {
+
         if (driver != null) {
             driver.quit();
         }
--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -9,20 +9,28 @@ public class ProductPage extends BasePage {
     private By productsBtn = By.xpath("//a[contains(text(),'Products')]");
     private By searchBox = By.id("search_product");
     private By searchBtn = By.id("submit_search");
+
     private By firstAddToCart = By.xpath("(//a[contains(text(),'Add to cart')])[1]");
     private By continueShopping = By.xpath("//button[contains(text(),'Continue Shopping')]");
     private By cartBtn = By.xpath("//a[contains(text(),'Cart')]");
+
+    private By womenCategory = By.xpath("//a[contains(text(),'Women')]");
+    private By firstCategoryProduct = By.xpath("(//div[@class='features_items']//div[@class='product-image-wrapper'])[1]");
+
+    private By firstViewProduct = By.xpath("(//a[contains(text(),'View Product')])[1]");
+    private By productName = By.xpath("//div[@class='product-information']/h2");
+    private By productPrice = By.xpath("//div[@class='product-information']/span/span");
 
     public ProductPage(WebDriver driver) {
         super(driver);
     }
 
-    public void openProductPage() {
+    public void openProductsPage() {
         click(productsBtn);
     }
 
-    public void searchProduct(String productName) {
-        type(searchBox, productName);
+    public void searchProduct(String productNameText) {
+        type(searchBox, productNameText);
         click(searchBtn);
     }
 
@@ -36,5 +44,25 @@ public class ProductPage extends BasePage {
 
     public void goToCart() {
         click(cartBtn);
+    }
+
+    public void openWomenCategory() {
+        click(womenCategory);
+    }
+
+    public boolean isCategoryProductVisible() {
+        return isDisplayed(firstCategoryProduct);
+    }
+
+    public void openFirstProductDetail() {
+        click(firstViewProduct);
+    }
+
+    public String getProductName() {
+        return getText(productName);
+    }
+
+    public String getProductPrice() {
+        return getText(productPrice);
     }
 }
